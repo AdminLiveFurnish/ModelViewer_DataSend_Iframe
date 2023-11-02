0afd5c500786c907645d26772ba4d1c01a31fb3c
Jatin Sharma <97681208+jatinsharmalf@users.noreply.github.com>
2023-11-02 21:02:05 +0530

    update data & ui

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ModelViewer_DataSend_Iframe
+
+This project is an example of how to send a message to the model viewer iframe to let it know what option is selected.
+
+## Installation
+
+1. Clone the repository.
+   git clone https://github.com/username/project-name.git
+
+2. Navigate to the project directory.
+cd project-name
+
+3. Install the dependencies.
+npm install
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     const iframe = document.querySelector("#modelViewerFrame");
     if(iframe) {
       iframe.contentWindow.postMessage(data, "*");
-      console.log("data sent to child : " + data);
+      console.log("data sent to child: " + data);
     }
   }
 
@@ -19,19 +19,19 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <iframe id="modelViewerFrame" src="https://mainecottage.imagine.io/product_2?mode=configurator" allowFullScreen />
+        <iframe id="modelViewerFrame" src="https://mainecottage.imagine.io/product_1" allowFullScreen />
       </div>
 
       <div className="container">
         <div className="row">
           <p className="title">Frame</p>
           <div className="col">
-            <button className="option" onClick={() => sendData({ id: 0, type: 'frame' })}>2 leg frame</button>
-            <button className="option" onClick={() => sendData({ id: 1, type: 'frame' })}>3 leg frame</button>
+            <button className="option" onClick={() => sendData({ id: 0, type: 'frame' })}>Stationary</button>
+            <button className="option" onClick={() => sendData({ id: 1, type: 'frame' })}>Swivel</button>
           </div>
         </div>
 
-        <div className="row">
+        {/* <div className="row">
           <p className="title">Cleg</p>
           <div className="col">
             <button className="option" onClick={() => sendData({ id: 0, type: 'leg' })}>Rectangle</button>
@@ -45,15 +45,27 @@ function App() {
             <button className="option" onClick={() => sendData({ id: 0, type: 'orientation' })}>Standard</button>
             <button className="option" onClick={() => sendData({ id: 1, type: 'orientation' })}>Reversed</button>
           </div>
+        </div> */}
+
+        <div className="row">
+          <p className="title">Upholstery</p>
+          <div className="col">
+            <button className="option" onClick={() => sendData({ id: 0, type: 'color' })}>Thatch: Bluebell</button>
+            <button className="option" onClick={() => sendData({ id: 2, type: 'color' })}>Boomerang: Cloud</button>
+            <button className="option" onClick={() => sendData({ id: 6, type: 'color' })}>Crazy Daisy: Nikko Blue</button>
+            <button className="option" onClick={() => sendData({ id: 22, type: 'color' })}>Grand Mum: Sun</button>
+          </div>
         </div>
 
         <div className="row">
-          <p className="title">Color</p>
+          <p className="title">Leg</p>
           <div className="col">
-            <button className="option" onClick={() => sendData({ id: 0, type: 'color' })}>Silver</button>
-            <button className="option" onClick={() => sendData({ id: 1, type: 'color' })}>Black</button>
+            <button className="option" onClick={() => sendData({ id: 93, type: 'color' })}>Nordic</button>
+            <button className="option" onClick={() => sendData({ id: 94, type: 'color' })}>Fruitwood</button>
+            <button className="option" onClick={() => sendData({ id: 95, type: 'color' })}>Java</button>
           </div>
         </div>
+
       </div>
     </div>
   );
