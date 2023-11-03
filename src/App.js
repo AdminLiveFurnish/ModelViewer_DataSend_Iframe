7dc6bc05597db914f86001b39c4a0af31800109d
Akram Azaan <akram.azaan@gmail.com>
2023-11-03 22:46:46 +0530

    type update
    updated type to send option based on metadata saved on shoppify

--- src/App.js
+++ src/App.js
@@ -26,43 +26,27 @@ function App() {
         <div className="row">
           <p className="title">Frame</p>
           <div className="col">
-            <button className="option" onClick={() => sendData({ id: 0, type: 'frame' })}>Stationary</button>
-            <button className="option" onClick={() => sendData({ id: 1, type: 'frame' })}>Swivel</button>
+            <button className="option" onClick={() => sendData({ id: 0, type: 'Movement' })}>Stationary</button>
+            <button className="option" onClick={() => sendData({ id: 1, type: 'Movement' })}>Swivel</button>
           </div>
         </div>
-
-        {/* <div className="row">
-          <p className="title">Cleg</p>
-          <div className="col">
-            <button className="option" onClick={() => sendData({ id: 0, type: 'leg' })}>Rectangle</button>
-            <button className="option" onClick={() => sendData({ id: 1, type: 'leg' })}>Round</button>
-          </div>
-        </div>
-
-        <div className="row">
-          <p className="title">Column Orientation</p>
-          <div className="col">
-            <button className="option" onClick={() => sendData({ id: 0, type: 'orientation' })}>Standard</button>
-            <button className="option" onClick={() => sendData({ id: 1, type: 'orientation' })}>Reversed</button>
-          </div>
-        </div> */}
 
         <div className="row">
           <p className="title">Upholstery</p>
           <div className="col">
-            <button className="option" onClick={() => sendData({ id: 0, type: 'color' })}>Thatch: Bluebell</button>
-            <button className="option" onClick={() => sendData({ id: 2, type: 'color' })}>Boomerang: Cloud</button>
-            <button className="option" onClick={() => sendData({ id: 6, type: 'color' })}>Crazy Daisy: Nikko Blue</button>
-            <button className="option" onClick={() => sendData({ id: 22, type: 'color' })}>Grand Mum: Sun</button>
+            <button className="option" onClick={() => sendData({ id: 0, type: 'Upholstery' })}>Thatch: Bluebell</button>
+            <button className="option" onClick={() => sendData({ id: 2, type: 'Upholstery' })}>Boomerang: Cloud</button>
+            <button className="option" onClick={() => sendData({ id: 6, type: 'Upholstery' })}>Crazy Daisy: Nikko Blue</button>
+            <button className="option" onClick={() => sendData({ id: 22, type: 'Upholstery' })}>Grand Mum: Sun</button>
           </div>
         </div>
 
         <div className="row">
           <p className="title">Leg</p>
           <div className="col">
-            <button className="option" onClick={() => sendData({ id: 93, type: 'color' })}>Nordic</button>
-            <button className="option" onClick={() => sendData({ id: 94, type: 'color' })}>Fruitwood</button>
-            <button className="option" onClick={() => sendData({ id: 95, type: 'color' })}>Java</button>
+            <button className="option" onClick={() => sendData({ id: 93, type: 'Leg Finish' })}>Nordic</button>
+            <button className="option" onClick={() => sendData({ id: 94, type: 'Leg Finish' })}>Fruitwood</button>
+            <button className="option" onClick={() => sendData({ id: 95, type: 'Leg Finish' })}>Java</button>
           </div>
         </div>
 
