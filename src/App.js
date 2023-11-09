bfe10c1e5f24b4454a23c16a1a2a53a222b53e4a
Akram Azaan <akram.azaan@gmail.com>
2023-11-09 10:04:16 +0530

    updated to use name instead of ID

--- src/App.js
+++ src/App.js
@@ -19,34 +19,28 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <iframe id="modelViewerFrame" src="https://mainecottage.imagine.io/product_1" allowFullScreen />
+        <iframe id="modelViewerFrame" src="https://mainecottage.imagine.io/3" allowFullScreen />
+        {/* <iframe id="modelViewerFrame" src="http://localhost:3000/1" allowFullScreen /> */}
       </div>
 
       <div className="container">
-        <div className="row">
-          <p className="title">Frame</p>
-          <div className="col">
-            <button className="option" onClick={() => sendData({ id: 0, type: 'Movement' })}>Stationary</button>
-            <button className="option" onClick={() => sendData({ id: 1, type: 'Movement' })}>Swivel</button>
-          </div>
-        </div>
 
         <div className="row">
           <p className="title">Upholstery</p>
           <div className="col">
-            <button className="option" onClick={() => sendData({ id: 0, type: 'Upholstery' })}>Thatch: Bluebell</button>
-            <button className="option" onClick={() => sendData({ id: 2, type: 'Upholstery' })}>Boomerang: Cloud</button>
-            <button className="option" onClick={() => sendData({ id: 6, type: 'Upholstery' })}>Crazy Daisy: Nikko Blue</button>
-            <button className="option" onClick={() => sendData({ id: 22, type: 'Upholstery' })}>Grand Mum: Sun</button>
+            <button className="option" onClick={() => sendData({ name: "Thatch : Bluebell", type: 'Upholstery' })}>Thatch: Bluebell</button>
+            <button className="option" onClick={() => sendData({ name: "Boomerang: Cloud", type: 'Upholstery' })}>Boomerang: Cloud</button>
+            <button className="option" onClick={() => sendData({ name: "Crazy Daisy : Nikko Blue", type: 'Upholstery' })}>Crazy Daisy: Nikko Blue</button>
+            <button className="option" onClick={() => sendData({ name: "Grand Mum : Sun", type: 'Upholstery' })}>Grand Mum: Sun</button>
           </div>
         </div>
 
         <div className="row">
           <p className="title">Leg</p>
           <div className="col">
-            <button className="option" onClick={() => sendData({ id: 93, type: 'Leg Finish' })}>Nordic</button>
-            <button className="option" onClick={() => sendData({ id: 94, type: 'Leg Finish' })}>Fruitwood</button>
-            <button className="option" onClick={() => sendData({ id: 95, type: 'Leg Finish' })}>Java</button>
+            <button className="option" onClick={() => sendData({ name: "Nordic", type: 'Leg Finish' })}>Nordic</button>
+            <button className="option" onClick={() => sendData({ name: "Fruitwood", type: 'Leg Finish' })}>Fruitwood</button>
+            <button className="option" onClick={() => sendData({ name: "Java", type: 'Leg Finish' })}>Java</button>
           </div>
         </div>
 
