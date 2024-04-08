0f24cc1329d80749ab9d2300175e862b96dec998
Mayank Jaiswal <mayankjaiswal@Mayanks-MacBook-Pro.local>
2024-04-08 13:22:51 +0530

    scene change

--- src/App.js
+++ src/App.js
@@ -35,16 +35,24 @@ function App() {
     <div className="App">
       <div className="container">
         {/* Add the link to the model viewer iframe here */}
-        <iframe id="modelViewerFrame" src="http://localhost:3000/8" allowFullScreen />
+        <iframe id="modelViewerFrame" src="http://localhost:3000/configurator/1" allowFullScreen />
       </div>
 
       <div className="container">
+      <div className="row">
+          <p className="title">Switch between scenes</p>
+          <div className="col">
+          <button className="option" onClick={() => sendData({ type: {id: 1913, name: 'Princess'}, updateScene: true })}>Scene 1</button>
+            <button className="option" onClick={() => sendData({ type: {id: 1915, name: 'Princess Ottomon'}, updateScene: true })}>Scene 2</button>
+          </div>
+        </div>
 
         <div className="row">
           <p className="title">Upholstery</p>
           <div className="col">
             {/* send information is a json format to the iframe here */}
-            <button className="option" onClick={() => sendData({ name: "Thatch : Bluebell", type: 'Upholstery' })}>Thatch: Bluebell</button>
+            <button className="option" onClick={() => sendData({ label: 'Frame', option: 'Infinity Grey' })}>Infinity Grey</button>
+            <button className="option" onClick={() => sendData({ label: 'Frame', option: 'Infinity Amber' })}>Infinity Amber</button>
             <button className="option" onClick={() => sendData({ name: "Boomerang: Cloud", type: 'Upholstery' })}>Boomerang: Cloud</button>
             <button className="option" onClick={() => sendData({ name: "Crazy Daisy : Nikko Blue", type: 'Upholstery' })}>Crazy Daisy: Nikko Blue</button>
             <button className="option" onClick={() => sendData({ name: "Grand Mum : Sun", type: 'Upholstery' })}>Grand Mum: Sun</button>
@@ -54,8 +62,8 @@ function App() {
         <div className="row">
           <p className="title">Leg</p>
           <div className="col">
-            <button className="option" onClick={() => sendData({ name: "Nordic", type: 'Leg Finish' })}>Nordic</button>
-            <button className="option" onClick={() => sendData({ name: "Fruitwood", type: 'Leg Finish' })}>Fruitwood</button>
+            <button className="option" onClick={() => sendData({ label: "Legs", option: 'Walnut' })}>Walnut</button>
+            <button className="option" onClick={() => sendData({ label: "Legs", option: 'Oak' })}>Oak</button>
             <button className="option" onClick={() => sendData({ name: "Java", type: 'Leg Finish' })}>Java</button>
           </div>
         </div>
